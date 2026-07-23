47180937c07e70e1775c9b220fe0ab52edeb6a8e
ReggieGrant <grantreggie09@gmail.com>
2026-07-22 17:23:11 -0700

    Initial Commit

---
+++ TaskFlow/TaskFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskFlowApp.swift
+//  TaskFlow
+//
+//  Created by Reginald Grant on 7/22/26.
+//
+
+import SwiftUI
+
+@main
+struct TaskFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
